a5ba18845f2774f5f729c4cc9e6ba9014bfc60cf
Devi <devi131780@gmail.com>
2025-08-13 12:34:36 +0530

    adding two integers

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ function printHello() {
   console.log("Hello World!");
 }
 printHello();
+
+function addTwoIntegers(a, b) {
+  return a + b;
+}
+let sum = addTwoIntegers(4, 5);
+console.log("The sum of given numbers is " + sum);
