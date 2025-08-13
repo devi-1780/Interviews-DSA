49408ceb8769873e87cc821feadb3b98094528bd
Devi <devi131780@gmail.com>
2025-08-13 12:37:18 +0530

    find square of a given number

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ function addTwoIntegers(a, b) {
 }
 let sum = addTwoIntegers(4, 5);
 console.log("The sum of given numbers is " + sum);
+
+function findSquare(num) {
+  return num ** 2;
+}
+
+let square = findSquare(4);
+console.log("Square of the given number is " + square);
